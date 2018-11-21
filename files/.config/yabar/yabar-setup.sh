4d7409a0b1cdff1d2cafd3dc152511befcb1e991
schutzekatze <vladimir.nik94@gmail.com>
2018-11-21 21:46:03 +0000

    Improve yabar config

--- files/.config/yabar/yabar-setup.sh
+++ files/.config/yabar/yabar-setup.sh
@@ -4,9 +4,14 @@ source ~/.cache/wal/colors.sh
 
 config=$(cat ~/.config/yabar/yabar.conf)
 
+wifi=$(ls /sys/class/net | grep -m 1 wl)
+device=$(df -h . | head -2 | tail -1 | awk '{print $1}')
+
 config=$(echo "$config" \
 	| sed "s~<background_argb>~0xD8${color0: -6}~g" \
 	| sed "s~<foreground_rgb>~0x${color2: -6}~g" \
+	| sed "s~<wifi>~$wifi~g" \
+	| sed "s~<device>~$device~g" \
 	)
 
 echo "$config" >/tmp/yabar.conf
