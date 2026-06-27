33f644bc17fb075e88e9140320195c217f0723a6
vlad0x00 <vladlich@protonmail.com>
2026-06-27 15:29:34 -0700

    Update

---
+++ files/.config/fish/conf.d/alias.fish
@@ -0,0 +1,11 @@
+status is-interactive; or return
+
+alias mv 'mv -i'
+alias cp 'cp -i'
+alias rm 'rm -i'
+
+alias gst 'git status'
+alias gp  'git push'
+alias gcb 'git checkout -b'
+alias gl  'git pull'
+alias gd  'git diff'
