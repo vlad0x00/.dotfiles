047af7cefd16d4c589f85476f4edd72b2c788dc3
vlad0x00 <vladlich@protonmail.com>
2026-06-27 15:50:10 -0700

    Update

---
+++ files/.config/fish/conf.d/aesthetics.fish
@@ -0,0 +1,3 @@
+status is-interactive; or return
+
+test -e ~/.cache/wal/sequences && cat ~/.cache/wal/sequences
--- files/.config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-set -g fish_greeting
-
-if status is-interactive
-  test -e ~/.cache/wal/sequences && cat ~/.cache/wal/sequences
-end
-
-zoxide init fish | source
